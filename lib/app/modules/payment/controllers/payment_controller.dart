8a4af271f7076b2d83bbccb4419d60c2dd9a8472
aslahmp <aslahmp@gmail.com>
2022-03-28 10:56:56 +0530

    payment

--- lib/app/modules/payment/controllers/payment_controller.dart
+++ lib/app/modules/payment/controllers/payment_controller.dart
@@ -1,12 +1,25 @@
 import 'package:get/get.dart';
 
-class PaymentController extends GetxController {
-  //TODO: Implement PaymentController
+import '../../../../infastructure/managers/index.dart';
+import '../../../data/models/pament/payment/yearly_payment.model.dart';
 
-  final count = 0.obs;
+class PaymentController extends GetxController {
+  var paymentdetails = YearlyUserPaymentModel().obs;
+  var selectedYear = YearManager.currentYear.obs;
+  var paymentFirbase = GetPaymentFirbase();
   @override
   void onInit() {
     super.onInit();
+  }
+
+  void getPaymentData() async {
+    var result = await paymentFirbase.getUserYearPayment(
+        userId: studentModel.id, year: selectedYear.value);
+    if (result.isSuccess) {
+      paymentdetails.value = YearlyUserPaymentModel.fromjson(result.success);
+    } else {
+      paymentdetails.value = YearlyUserPaymentModel();
+    }
   }
 
   @override
@@ -16,5 +29,4 @@ class PaymentController extends GetxController {
 
   @override
   void onClose() {}
-  void increment() => count.value++;
 }
